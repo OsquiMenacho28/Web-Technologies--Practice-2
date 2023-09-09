544a3eb84f86ee448220b38a7c2ceed461230d6b
OsquiMenacho28 <osqui.menacho2002@gmail.com>
2023-09-09 13:53:21 -0400

    Completed Exercise

--- src/Ejercicio 1/script.js
+++ src/Ejercicio 1/script.js
@@ -1,7 +1,8 @@
-$(document).ready(function() {
-    $('.dropdown-item rounded-2 active').on('click', function() {
-        var caption = $('.dropdown-item rounded-2 active').val();
-        Swal.fire('Button pressed', 'You pressed the button with the caption: ' + caption, 'success')
+$(function() {
+    $('.dropdown-item').each(function() {
+        $(this).on('click', function() {
+            var caption = $(this).text();
+            Swal.fire('Opción seleccionada', 'Muy bien! Escogiste el premio: ' + caption, 'success');
+        });
     });
-    console.log($('.dropdown-item rounded-2 active').val());
 });
