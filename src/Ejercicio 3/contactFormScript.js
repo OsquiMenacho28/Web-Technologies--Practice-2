3f35a451c95ed8ac7c00a613f7ce6384f16b65d6
OsquiMenacho28 <osqui.menacho2002@gmail.com>
2023-09-12 03:35:45 -0400

    Exercise 3 Finished

--- src/Ejercicio 3/contactFormScript.js
+++ src/Ejercicio 3/contactFormScript.js
@@ -1,32 +1,47 @@
-/*function validate() {
-    var userName = document.getElementsByName("userName")[0].value;
-    var userEmail = document.getElementsByName("userEmail")[0].value;
-    var userPassword = document.getElementsByName("userPassword")[0].value;
-    var errorMessage = document.getElementById("lblMessage");
-    errorMessage.innerText = '';
-    if (userName === '') {
-        errorMessage.innerText = 'Error, ingrese su nombre por favor.';
-    }
-    else if (!/^[A-Za-z\s]+$/.test(userName)) {
-        errorMessage.innerText = 'Error, el nombre ingresado no es válido.';
-    }
-    else if (userEmail === '') {
-        errorMessage.innerText = 'Error, ingrese su correo electrónico por favor.';
-    }
-    else if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(userEmail)) {
-        errorMessage.innerText = 'Error, el correo electrónico ingresado no es válido.';
-    }
-    else if (userPassword === '') {
-        errorMessage.innerText = 'Error, ingrese su contraseña por favor.';
-    }
-    else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(userPassword)) {
-        errorMessage.innerText = 'Error, la contraseña debe contener minimamente 8 caracteres, una mayúscula, una minúscula y un número.';
-    }
-    else {
-        document.getElementsByName("userName")[0].value = '';
-        document.getElementsByName("userEmail")[0].value = '';
-        document.getElementsByName("userPassword")[0].value = '';
-        errorMessage.innerText = '';
-        alert("¡Bienvenid@ " + userName + "!");
-    }
-}*/
+$(function() {
+    var contactForm = $('[name="contactForm"]');
+    contactForm.on('submit', function(event) {
+        var userName = $('[name="userName"]').val();
+        var userEmail = $('[name="userEmail"]').val();
+        var userMessage = $('[name="userMessage"]').val();
+        event.preventDefault();
+        if (userName === '') {
+            showMessage('Error', 'Ingrese su nombre por favor.', 'error');
+        }
+        else if (!/^[A-Za-z\s]+$/.test(userName)) {
+            showMessage('Error', 'El nombre ingresado no es válido.', 'error');
+        }
+        else if (userEmail === '') {
+            showMessage('Error', 'Ingrese su correo electrónico por favor.', 'error');
+        }
+        else if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(userEmail)) {
+            showMessage('Error', 'El correo electrónico ingresado no es válido.', 'error');
+        }
+        else if (userMessage === '') {
+            showMessage('Error', 'Ingrese un mensaje por favor.', 'error');
+        }
+        else if (!/^.{10,}$/.test(userMessage)) {
+            showMessage('Error', 'El mensaje debe contener minimamente 10 caracteres.', 'error');
+        }
+        else {
+            showMessage(`¡Bienvenid@ ${userName}!`, 'Sus datos fueron registrados correctamente.', 'success');
+            $('[name="userName"]').val('');
+            $('[name="userEmail"]').val('');
+            $('[name="userMessage"]').val('');
+        }
+    });
+});
+
+function showMessage(messageTitle, messageText, messageIcon) {
+    Swal.fire({
+        title: messageTitle,
+        text: messageText,
+        icon: messageIcon,
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+        }
+    });
+}
